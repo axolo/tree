3069619fa6464ee7eda6241ed8d1972508aa0aa5
axolo <zergdo@gmail.com>
2026-06-03 12:03:47 +0800

    ci: config build

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+export default {
+  build: {
+    minify: 'terser',
+    lib: {
+      entry: 'src/tree.js',
+      name: 'Tree',
+      formats: ['es', 'umd'],
+      fileName: format => `tree.${format}.js`
+    },
+  }
+}
